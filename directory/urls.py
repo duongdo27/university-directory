a51e088aa346d60737c4ef3c92617e0b6ce6a87c
Duong Do <dohoangduong95@gmail.com>
2020-04-04 16:35:55 -0400

    Add department details

--- directory/urls.py
+++ directory/urls.py
@@ -8,5 +8,6 @@ from directory import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('/department_list', views.DepartmentListView.as_view(), name='department_list'),
+    path('department_list', views.DepartmentListView.as_view(), name='department_list'),
+    path('department/<int:pk>', views.DepartmentDetailView.as_view(), name='department_detail'),
 ]
--- directory/views.py
+++ directory/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from directory.models import Department
 
@@ -14,4 +15,12 @@ class DepartmentListView(ListView):
 
     def get_queryset(self):
         return Department.objects.all().order_by('name')
+
+
+class DepartmentDetailView(DetailView):
+    template_name = "department/detail.html"
+    context_object_name = 'department'
+    model = Department
+
+
 
