cfcbd378ef5bad4c989ea1caadfcff21b3a27cfb
Duong Do <dohoangduong95@gmail.com>
2020-04-04 16:14:08 -0400

    Add department view

---
+++ directory/urls.py
@@ -0,0 +1,12 @@
+"""
+URLs for University app
+"""
+#pylint: disable=invalid-name
+from django.urls import path
+
+from directory import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('/department_list', views.DepartmentListView.as_view(), name='department_list'),
+]
--- directory/views.py
+++ directory/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-# Create your views here.
+from directory.models import Department
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+class DepartmentListView(ListView):
+    template_name = "department/index.html"
+    context_object_name = 'departments'
+
+    def get_queryset(self):
+        return Department.objects.all().order_by('name')
+
